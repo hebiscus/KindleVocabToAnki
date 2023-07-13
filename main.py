f44f6f2f020411eccec8c189d92f242307281499
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-13 14:20:04 +0200

    get rid of en: in words

--- main.py
+++ main.py
@@ -5,47 +5,29 @@ connection = sqlite3.connect("vocab.db")
 
 cursor = connection.cursor()
 
-# cursor.execute("SELECT * FROM WORDS")
+def deleteDuplicates():
+    cursor.execute("DELETE FROM LOOKUPS WHERE EXISTS (SELECT 1 FROM LOOKUPS P2 WHERE LOOKUPS.word_key = p2.word_key AND LOOKUPS.rowid > p2.rowid)")
 
-# threeWords = cursor.fetchmany(3)
+def deleteEN():
+    cursor.execute("UPDATE LOOKUPS SET word_key = SUBSTRING(word_key, 4)")
 
-# cursor.execute("SELECT * FROM LOOKUPS")
+def getUpdatedWords():
+    cursor.execute("SELECT word_key, usage FROM LOOKUPS")
 
-# threeContext = cursor.fetchmany(3)
-
-# def list_three():
-#     for x in range(3):
-#         print("stem:" + " " + threeWords[x][2] + " " + "context:" + " " + threeContext[x][5])
-
-# list_three()
-
-
-# cursor.execute("SELECT word_key, COUNT(*) AS CNT FROM LOOKUPS GROUP BY word_key HAVING COUNT(*) > 1")
-
-# cursor.execute("SELECT DISTINCT word_key FROM LOOKUPS")
-
-# print(cursor.fetchall())
-
-# cursor.execute("SELECT COUNT(DISTINCT word_key), usage FROM LOOKUPS")
-
-cursor.execute("DELETE FROM LOOKUPS WHERE EXISTS (SELECT 1 FROM LOOKUPS P2 WHERE LOOKUPS.word_key = p2.word_key AND LOOKUPS.rowid > p2.rowid)")
-
-cursor.execute("SELECT COUNT(word_key) FROM LOOKUPS")
-# print(cursor.fetchall())
-
-cursor.execute("SELECT word_key, usage FROM LOOKUPS")
+deleteDuplicates()
+deleteEN()
+getUpdatedWords()
 nonDuplicates = cursor.fetchall()
 
 def printNonDuplicates():
     for word in nonDuplicates:
         print(word)
 
-printNonDuplicates()
+# printNonDuplicates()
 
 def ImportToExcel():
     df = pandas.DataFrame(nonDuplicates, columns=["Word", "Context"])
     df.to_excel('result.xlsx', index = False)
-
 
 ImportToExcel()
 
