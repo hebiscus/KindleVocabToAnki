7ed6f5b410f234afa7d6cc36ffed404cf4715866
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-10-22 16:51:52 +0200

    upload vocab file basic, text response

--- frontend/src/components/homepage.tsx
+++ frontend/src/components/homepage.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 
 function Homepage() {
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const uploadVocab = async(e: React.FormEvent) => {
+    e.preventDefault();
+
+    const data = new FormData();
+    data.append("file", selectedFile, "vocab.db");
+
+    fetch("http://localhost:8000/upload", { 
+      method: 'POST',
+      body: data,
+    })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
   return (
-    <div>home</div>
+    <>
+      <form >
+        <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])}/>
+        <button onClick={uploadVocab}>yes</button>
+      </form>
+    </>
   )
 }
 
