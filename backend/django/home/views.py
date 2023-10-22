c96683ba4c27ef526f62e56b03bf944a5e7be7e1
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-10-22 15:22:34 +0200

    delete unused imports

--- backend/django/home/views.py
+++ backend/django/home/views.py
@@ -1,6 +1,4 @@
-from django.http import JsonResponse
 from django.http import FileResponse
-from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
