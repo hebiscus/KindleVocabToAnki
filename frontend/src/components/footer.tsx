d7fe32f0d7a02829ee947fda285f6008de35ee37
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-10-28 17:57:01 +0200

    add styling homepage footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,7 +2,10 @@
 function Footer() {
   return (
     <footer className="text-center text-2xl">
-      <span>hebiscus</span>
+      <a className="flex justify-center items-center gap-1 hover:text-purple-700" href="https://github.com/hebiscus">
+        <img className="max-h-9" src="./github-mark.svg" alt=""/>
+        hebiscus
+      </a>
     </footer>
   )
 }
--- frontend/src/components/homepage.tsx
+++ frontend/src/components/homepage.tsx
@@ -28,14 +28,14 @@ function Homepage() {
       <div className="absolute top-1/2 -translate-y-2/3 max-w-lg self-center text-3xl flex flex-col items-center gap-3">
         <span>Upload your vocab.db file:</span>
         <form className="flex w-full justify-around">
-          <label className="px-1.5 py-3.5 rounded-xl shadow-md shadow-purple-300 border-black border border-solid hover:cursor-pointer" htmlFor="dbFile">Choose a file</label>
+          <label className="px-1.5 py-3.5 rounded-xl shadow-md shadow-purple-300 border-black border border-solid hover:cursor-pointer hover:scale-110" htmlFor="dbFile">Choose a file</label>
           <input className="hidden" type="file" id="dbFile" onChange={(e) => setSelectedFile(e.target!.files?.[0])}/>
-          <button className="" type="submit" onClick={uploadVocab}>Upload</button>
+          <button className="text-purple-700 hover:text-purple-800" type="submit" onClick={uploadVocab}>Upload</button>
         </form>
         <span className="text-2xl">Don't know how to get it?</span>
-        <span className="text-2xl">Visit <a className="text-purple-700 underline" href="">Instructions</a></span>
+        <span className="text-2xl">Visit <a className="text-purple-700 underline hover:text-purple-800" href="">Instructions</a></span>
+        {fileDownload && <a className="text-purple-700 font-semibold my-3 hover:text-purple-800" href={fileDownload} download="words.csv">Download</a>}
       </div>
-      {fileDownload && <a href={fileDownload} download="words.csv">Download</a>}
     </>
   )
 }
