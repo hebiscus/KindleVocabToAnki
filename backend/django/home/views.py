68622aaa0100c450a7ee062d27cd71b6c71346d0
hebiscus <akikopoland@gmail.com>
2025-03-09 20:55:12 +0100

    chore: apply black linter

--- backend/django/home/views.py
+++ backend/django/home/views.py
@@ -10,54 +10,58 @@ from rest_framework.response import Response
 
 from django.http import FileResponse
 
-nltk.download('wordnet')
+nltk.download("wordnet")
 from nltk.corpus import wordnet
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def upload_vocab(request):
-  vocab_file = request.FILES['file']
+    vocab_file = request.FILES["file"]
 
-  if vocab_file and vocab_file.name.endswith('.db'):
-    try: 
-      with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        for chunk in vocab_file.chunks():
-          temp_file.write(chunk)
-      
-      connection = sqlite3.connect(temp_file.name)
-      cursor = connection.cursor()
+    if vocab_file and vocab_file.name.endswith(".db"):
+        try:
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                for chunk in vocab_file.chunks():
+                    temp_file.write(chunk)
 
-      cursor.execute("DELETE FROM LOOKUPS WHERE EXISTS (SELECT 1 FROM LOOKUPS P2 WHERE LOOKUPS.word_key = p2.word_key AND LOOKUPS.rowid > p2.rowid)")
-      cursor.execute("UPDATE LOOKUPS SET word_key = SUBSTRING(word_key, 4)")
-      cursor.execute("SELECT word_key, usage FROM LOOKUPS")
+            connection = sqlite3.connect(temp_file.name)
+            cursor = connection.cursor()
 
-      nonDuplicates = cursor.fetchall()
+            cursor.execute(
+                "DELETE FROM LOOKUPS WHERE EXISTS (SELECT 1 FROM LOOKUPS P2 WHERE LOOKUPS.word_key = p2.word_key AND LOOKUPS.rowid > p2.rowid)"
+            )
+            cursor.execute("UPDATE LOOKUPS SET word_key = SUBSTRING(word_key, 4)")
+            cursor.execute("SELECT word_key, usage FROM LOOKUPS")
 
-      connection.commit()
-      connection.close()
+            nonDuplicates = cursor.fetchall()
 
-      df = pandas.DataFrame(nonDuplicates, columns=["Word", "Context"])
-      df['Definitions'] = df.apply(getDefinitions, axis=1)
-      df.to_csv('result.csv', index = False)
+            connection.commit()
+            connection.close()
 
-    except Exception as e:
-      return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    finally:
-      temp_file.close()
-      os.unlink(temp_file.name)
+            df = pandas.DataFrame(nonDuplicates, columns=["Word", "Context"])
+            df["Definitions"] = df.apply(getDefinitions, axis=1)
+            df.to_csv("result.csv", index=False)
 
-  return FileResponse(open('result.csv', 'rb'))
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        finally:
+            temp_file.close()
+            os.unlink(temp_file.name)
+
+    return FileResponse(open("result.csv", "rb"))
 
 
 def getDefinitions(row):
-    word = row['Word']
+    word = row["Word"]
     if " " in word:
-      word = word.replace(" ", "_")
+        word = word.replace(" ", "_")
 
     synset_array = wordnet.synsets(word)
     try:
-      definition = synset_array[0].definition()
+        definition = synset_array[0].definition()
     except IndexError:
-      definition = 'definition not found'
-    
+        definition = "definition not found"
+
     return definition
