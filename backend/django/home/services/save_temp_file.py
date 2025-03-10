7e67971b1843cf2276ba401d6886fb5d44bd9855
hebiscus <akikopoland@gmail.com>
2025-03-10 23:10:12 +0100

    feat: create save temp file

---
+++ backend/django/home/services/save_temp_file.py
@@ -0,0 +1,13 @@
+import tempfile
+
+def save_temp_file(vocab_file):
+    if vocab_file and vocab_file.name.endswith('.db'):
+        try:
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                for chunk in vocab_file.chunks():
+                    temp_file.write(chunk)
+                return temp_file.name
+        except Exception as e:
+            raise Exception(f"Error saving the uploaded file: {str(e)}")
+    else:
+        raise Exception("Invalid file format. Only .db files are allowed.")
