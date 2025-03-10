0330a95c7d5581cc8ec439c94d208f4ac8830deb
hebiscus <akikopoland@gmail.com>
2025-03-10 23:44:39 +0100

    fix: delete duplicated handle definition

--- backend/django/home/management/commands/download_wordnet.py
+++ backend/django/home/management/commands/download_wordnet.py
@@ -5,7 +5,6 @@ class Command(BaseCommand):
     help = "Downloads wordnet nltk dictionary database"
 
     def handle(self, *args, **options):
-        def handle(self, *args, **kwargs):
           nltk.download('wordnet')
           self.stdout.write(self.style.SUCCESS('Successfully downloaded wordnet'))
 
