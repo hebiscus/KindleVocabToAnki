e0ea4b3b338cf7287655a18ef7e8b12721e81943
hebiscus <akikopoland@gmail.com>
2025-03-10 23:45:00 +0100

    ref: change wording

--- backend/django/home/views.py
+++ backend/django/home/views.py
@@ -1,5 +1,5 @@
 from .services.save_temp_file import save_temp_file
-from .services.get_unique_words import get_non_duplicates
+from .services.get_unique_words import get_unique_words
 from .services.generate_csv import generate_csv
 from django.http import FileResponse
 from rest_framework.response import Response
@@ -14,8 +14,8 @@ def upload_vocab(request):
 
   try:
       db_file_path = save_temp_file(vocab_file)
-      non_duplicates = get_non_duplicates(db_file_path)
-      generate_csv(non_duplicates)
+      unique_words = get_unique_words(db_file_path)
+      generate_csv(unique_words)
 
       return FileResponse(open('result.csv', 'rb'))
 
